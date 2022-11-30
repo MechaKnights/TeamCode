0bd016d53411e4f512d4ab725fc67fe71fbc8d03
MechaKnights <113733034+MechaKnights@users.noreply.github.com>
2022-11-30 16:29:23 -0700

    Update WebcamTest.java

--- teamcode/WebcamTest.java
+++ teamcode/WebcamTest.java
@@ -4,10 +4,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.teamcode.SignalPipeline;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @Autonomous(name = "Webcam Test")
 public class WebcamTest extends LinearOpMode {
@@ -15,6 +15,12 @@ public class WebcamTest extends LinearOpMode {
     SignalPipeline SignalPipeline = new SignalPipeline();
     OpenCvCamera camera;
     String webcamName = "Webcam 1";
+    org.firstinspires.ftc.teamcode.SignalPipeline pipeline;
+    org.firstinspires.ftc.teamcode.SignalPipeline.TeamShippingElementPosition snapshotAnalysis = org.firstinspires.ftc.teamcode.SignalPipeline.TeamShippingElementPosition.RIGHT;
+
+
+    private DcMotorEx lift;
+    private DcMotorEx lift2;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -23,11 +29,9 @@ public class WebcamTest extends LinearOpMode {
         SignalPipeline = new SignalPipeline();
         camera.setPipeline(SignalPipeline);
 
-        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
-            public void onOpened()
-            {
+            public void onOpened() {
                 camera.startStreaming(320,240, OpenCvCameraRotation.SIDEWAYS_LEFT);
             }
 
@@ -35,11 +39,47 @@ public class WebcamTest extends LinearOpMode {
             public void onError(int errorCode) {}
         });
 
-        while (!isStarted() && !isStopRequested()) {
-            telemetry.addData("ROTATION: ", SignalPipeline.getAnalysis());
+        while (!isStarted()) {
+            telemetry.addData("Realtime analysis", SignalPipeline.getAnalysis());
             telemetry.update();
 
             sleep(50);
+        }
+
+        waitForStart();
+        
+        snapshotAnalysis = SignalPipeline.getAnalysis();
+
+        telemetry.addData("Snapshot post-START analysis", snapshotAnalysis);
+        telemetry.update();
+        
+        while (opModeIsActive()) {
+            sleep(50);
+            switch (snapshotAnalysis) {
+                case LEFT: {
+                    lift.setPower(0.5);
+                    lift2.setPower(0.5);
+                    sleep(800);
+                    lift.setPower(0);
+                    lift2.setPower(0);
+                }
+                case CENTER: {
+                    lift.setPower(0.5);
+                    lift2.setPower(0.5);
+                    sleep(600);
+                    lift.setPower(0);
+                    lift2.setPower(0);
+
+                }
+                case RIGHT: {
+                    lift.setPower(0.5);
+                    lift2.setPower(0.5);
+                    sleep(400);
+                    lift.setPower(0);
+                    lift2.setPower(0);
+
+                }
+            }
         }
     }
 }
